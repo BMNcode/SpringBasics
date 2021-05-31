447ff2a58bbc169cb4c8c1e9a3b1545e66b147db
Максим <happymax@list.ru>
2021-05-31 23:24:54 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,19 @@
+public class App {
+
+    private Client client;
+    private ConsoleEventLogger eventLogger;
+
+    public App(Client client, ConsoleEventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    
+    private void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        eventLogger.logEvent(message);
+    }
+}
---
+++ src/main/java/ConsoleEventLogger.java
@@ -0,0 +1,6 @@
+public class ConsoleEventLogger implements EventLogger{
+    @Override
+    public void logEvent(String message) {
+        System.out.println(message);
+    }
+}
